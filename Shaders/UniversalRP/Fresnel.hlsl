d9029c69d8a0447257f38dfd64bb230aa782a307
Yuri <yuriaicruz@gmail.com>
2020-03-01 12:33:34 -0300

    hlsl shader study - fresnel

---
+++ Shaders/UniversalRP/Fresnel.hlsl
@@ -0,0 +1,20 @@
+﻿#ifndef MYRP_UNLIT_INCLUDED
+#define MYRP_UNLIT_INCLUDED
+
+struct appdata {
+	float4 pos : POSITION;
+	float2 uv : TEXCOORD;
+	float4 normal : NORMAL;
+};
+
+struct v2f {
+	float4 pos : SV_POSITION;
+	float4 normal : NORMAL;
+	float4 worldPos : TEXCOORD1;
+	float fresnel : TEXCOORD2;
+	float2 uv : TEXCOORD;
+};
+
+
+#endif // MYRP_UNLIT_INCLUDED
+
